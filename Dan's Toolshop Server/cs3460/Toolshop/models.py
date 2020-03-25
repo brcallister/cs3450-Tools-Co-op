57c371f5c81b0247784bd215c558f39f3bd36126
Brian Callister <brian.callister@aggiemail.usu.edu>
2020-03-25 13:20:59 -0600

    Updated the customer model to add 'outstanding balance'

--- Dan's Toolshop Server/cs3460/Toolshop/models.py
+++ Dan's Toolshop Server/cs3460/Toolshop/models.py
@@ -12,7 +12,7 @@ class Tool(models.Model):
     condition = models.TextField(blank=True)  # Blank here means "Can be blank" sometimes.
 
     is_checked_out = models.BooleanField(default=False)
-    date_checked_out = models.DateTimeField(blank='true', null=True)  # Blank\null here means "Can be blank" sometimes.
+    date_checked_out = models.DateTimeField(blank=True, null=True)  # Blank\null here means "Can be blank" sometimes.
     who_checked_out = models.CharField(max_length=100, blank=True)  # Blank here means "Can be blank" sometimes.
 
     def __str__(self):
@@ -25,5 +25,6 @@ class CustomerInfo(models.Model):
     address = models.TextField()
 
     this_period_paid = models.BooleanField(default=False)
-    date_paid_until = models.DateTimeField(blank='true', null=True)
+    date_paid_until = models.DateTimeField(blank=True, null=True)
     num_currently_checked_out = models.PositiveIntegerField()
+    current_outstanding_balance = models.PositiveIntegerField(blank=True)
